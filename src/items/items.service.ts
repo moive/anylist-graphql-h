4b6d93fa065de401de29019a427d1cd39bfc2c00
Moises Velasquez <moises007@gmail.com>
2025-10-16 19:43:38 -0500

    findOneBy

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateItemInput, UpdateItemInput } from './dto/inputs';
 import { Item } from './entities/item.entity';
 import { Repository } from 'typeorm';
@@ -16,12 +16,16 @@ export class ItemsService {
     return await this.itemRepository.save(newItem);
   }
 
-  findAll() {
-    return `This action returns all items`;
+  async findAll(): Promise<Item[]> {
+    return this.itemRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} item`;
+  async findOne(id: string): Promise<Item> {
+    const item = await this.itemRepository.findOneBy({ id });
+
+    if (!item) throw new NotFoundException(`Item with ID ${id} not found`);
+
+    return item;
   }
 
   update(id: number, updateItemInput: UpdateItemInput) {
