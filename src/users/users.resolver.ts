f3064ac653e538016388ef8646472e8cffcc4448
Moises Velasquez <moises007@gmail.com>
2025-11-18 23:56:29 -0500

    protected query user

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,16 +1,24 @@
+import { UseGuards } from '@nestjs/common';
 import { Resolver, Query, Mutation, Args, ID } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { ValidRolesArg } from './dto/args/roles.arg';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { ValidRoles } from 'src/auth/enums/valid-roles.enum';
 
 // import { UpdateUserInput } from './dto/update-user.input';
 
 @Resolver(() => User)
+@UseGuards(JwtAuthGuard)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Query(() => [User], { name: 'users' })
-  findAll(@Args() validRoles: ValidRolesArg): Promise<User[]> {
+  findAll(
+    @Args() validRoles: ValidRolesArg,
+    @CurrentUser([ValidRoles.admin]) user: User,
+  ): Promise<User[]> {
     return this.usersService.findAll(validRoles.roles);
   }
 
