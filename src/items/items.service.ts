97f767bf145454426ac15065a982bb11906df505
Moises Velasquez <moises007@gmail.com>
2025-11-21 18:03:26 -0500

    UpdateItem by User

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -36,7 +36,12 @@ export class ItemsService {
     return item;
   }
 
-  async update(id: string, updateItemInput: UpdateItemInput): Promise<Item> {
+  async update(
+    id: string,
+    updateItemInput: UpdateItemInput,
+    user: User,
+  ): Promise<Item> {
+    await this.findOne(id, user);
     const item = await this.itemsRepository.preload(updateItemInput);
 
     if (!item) throw new NotFoundException(`Item with ID ${id} not found`);
