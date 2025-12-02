623e68e0a3ce95d07a71e76505d9cbac698eea66
Moises Velasquez <moises007@gmail.com>
2025-12-02 17:40:54 -0500

    remove before saved data

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -3,9 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 
 import { SeedService } from './seed.service';
 import { SeedResolver } from './seed.resolver';
+import { UsersModule } from 'src/users/users.module';
+import { ItemsModule } from 'src/items/items.module';
 
 @Module({
   providers: [SeedResolver, SeedService],
-  imports: [ConfigModule],
+  imports: [ConfigModule, UsersModule, ItemsModule],
 })
 export class SeedModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,12 +1,24 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Item } from 'src/items/entities/item.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class SeedService {
   private isProd: boolean;
 
-  constructor(private readonly configService: ConfigService) {
-    this.isProd = this.configService.get('STATE') === 'prod';
+  constructor(
+    private readonly configService: ConfigService,
+    @InjectRepository(Item)
+    private readonly itemsRepository: Repository<Item>,
+
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {
+    this.isProd = configService.get('STATE') === 'prod';
   }
 
   async executedSeed() {
@@ -16,11 +28,28 @@ export class SeedService {
     }
 
     // cleanning database
+    await this.deleteDatabase();
 
     // Create users
 
     // Create items
 
     return true;
+  }
+
+  async deleteDatabase() {
+    // delete items
+    await this.itemsRepository
+      .createQueryBuilder()
+      .delete()
+      .where({})
+      .execute();
+
+    // delete users
+    await this.usersRepository
+      .createQueryBuilder()
+      .delete()
+      .where({})
+      .execute();
   }
 }
